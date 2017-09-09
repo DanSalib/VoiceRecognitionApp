6336eab4f0482b6675490de448361399934b974e
Daniel Salib <Daniel@Daniels-MacBook-Air.local>
2017-09-09 03:53:15 -0400

    created CircleButton.swift

---
+++ VoiceRecognitionApp/VoiceRecognitionApp/CircleButton.swift
@@ -0,0 +1,13 @@
+//
+//  CircleButton.swift
+//  VoiceRecognitionApp
+//
+//  Created by Daniel Salib on 2017-09-09.
+//  Copyright © 2017 Daniel Salib. All rights reserved.
+//
+
+import UIKit
+
+class CircleButton: NSObject {
+
+}
