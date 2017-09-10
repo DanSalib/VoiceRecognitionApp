7e983837e540ff95e4a8fa357298dc3a5651da0b
Daniel Salib <Daniel@Daniels-MacBook-Air.local>
2017-09-09 22:57:38 -0400

    added necessary methods to speech controller to analyze and output transcribed audio on button press

--- VoiceRecognitionApp/VoiceRecognitionApp/ViewController.swift
+++ VoiceRecognitionApp/VoiceRecognitionApp/ViewController.swift
@@ -14,12 +14,45 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var activitySpinner: UIActivityIndicatorView!
     @IBOutlet weak var transcriptionTextView: UITextView!
+    
+    var audioPlayer: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         activitySpinner.accessibilityElementsHidden = true
     }
 
+    func requestSpeechAuth() {
+        SFSpeechRecognizer.RequestAuthorization { authStatus in
+            if authStatus == SFSpeechRecognizerAuthorizationStatus.authorized{
+                if let path = bundle.main.urlForResource("test", withExtnsion: "m4a"){
+                    do{
+                        let sound = try AVAudioPlayer(contentOf: path)
+                        self.audioPlayer = sound
+                        sound.play()
+                        
+                    } catch{
+                        print("Error")
+                    }
+                    
+                    let recognizer = SFSpeechRecognizer();
+                    let request = SFSpeechRecognitionRequest(url: path)
+                    recognizer?.recognitionTask(with: request) { (result, error) in
+                        if let error = error {
+                            print("there was an error: \(error)")
+                        } else {
+                            print(result.bestTranscription.fomrattedString)
+                        }
+                    }
+                }
+            }
+        }
+    }
 
-
+    @IBAction func playBtnPressed(sender: AnyObject) {
+        activitySpinner.accessibilityElementsHidden = false
+        activitySpinner.startAnimating()
+        requestSpeechAuth()
+    }
 }
 
